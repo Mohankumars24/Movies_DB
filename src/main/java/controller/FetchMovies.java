64302805c798c8f9203503f2e08aff68d2211d4e
Mohankumars24 <mohankumars2412000@gmail.com>
2024-04-12 10:54:19 +0530

    Updated

--- src/main/java/controller/FetchMovies.java
+++ src/main/java/controller/FetchMovies.java
@@ -22,7 +22,35 @@ protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws Se
 		resp.getWriter().print("<h1 style='color:red' align='center'>No Movies Added</h1>");
 		req.getRequestDispatcher("home.html").include(req, resp);
 	}else {
-		
+		req.setAttribute("list", list);
+		req.getRequestDispatcher("FetchAll.jsp").include(req, resp);
+//		resp.getWriter().print("<html><body><div align='center'><table border='1'>");
+//
+//		resp.getWriter().print("<tr>");
+//		resp.getWriter().print("<th>Name</th>");
+//		resp.getWriter().print("<th>Image</th>");
+//		resp.getWriter().print("<th>Language</th>");
+//		resp.getWriter().print("<th>Genre</th>");
+//		resp.getWriter().print("<th>Rating</th>");
+//		resp.getWriter().print("<th>Edit</th>");
+//		resp.getWriter().print("<th>Delete</th>");
+//		resp.getWriter().print("</tr>");
+//
+//		for (Movie movie : list) {
+//			resp.getWriter().print("<tr>");
+//			resp.getWriter().print("<th>" + movie.getName() + "</th>");
+//			resp.getWriter().print("<th><img></th>");
+//			resp.getWriter().print("<th>" + movie.getLanguage() + "</th>");
+//			resp.getWriter().print("<th>" + movie.getGenre() + "</th>");
+//			resp.getWriter().print("<th>" + movie.getRating() + "</th>");
+//			resp.getWriter().print("<th><button>Edit</button></th>");
+//			resp.getWriter().print("<th><button>Delete</button></th>");
+//			resp.getWriter().print("</tr>");
+//		}
+//
+//		resp.getWriter().print("</table></div>");
+//		resp.getWriter().print("<div align='center'><a href='home.html'><button>Back</button></a></div>");
+//		resp.getWriter().print("</body></html>");
 	}
 }
 }
